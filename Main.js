6c502e794829b15a5712adb628c61b497f2e8541
nikita <nikitacohen7@gmail.com>
2022-05-25 01:02:05 +0300

    change index

--- Main.js
+++ Main.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const {Client} = require('@elastic/elasticsearch');
+const client = new Client({node : 'http://localhost:9200' })
 const cors = require('cors');
 const app = express();
 const searchRouter = require('./router/SearchRouter');
+
+client.indices.create({
+    index: "manualsfullindextwo",
+    body: {
+        "mappings": {
+            "aType": {
+                "properties": {
+                    "brand": {"type": "string", "index": "not_analyzed"},
+                    "category": {"type": "string", "index": "not_analyzed"},
+                    "url": {"type": "string", "index": "not_analyzed"},
+                    "title": {"type": "string", "index": "not_analyzed", unique: true}
+                }
+            }
+        }
+    }
+}, function (err, resp, respcode) {
+    console.log(err, resp, respcode);
+}).then() ;
 
 app.use(express.json());
 app.use(cors());
