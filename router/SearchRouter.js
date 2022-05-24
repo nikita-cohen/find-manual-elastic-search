90b848660300ca946c370339f4526dbc2cf47d52
nikita <nikitacohen7@gmail.com>
2022-05-25 00:08:41 +0300

    change index

--- router/SearchRouter.js
+++ router/SearchRouter.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/search/:name', searchController.getLimitedSearchResultController)
 router.get('/search/all/:name', searchController.getAllSearchResultController)
 router.post('/search/insert', searchController.insertManualController)
+router.post('/search/insert/two', searchController.insertManualController)
 
 module.exports = router;
