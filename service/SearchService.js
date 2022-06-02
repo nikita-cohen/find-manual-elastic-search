e7059cb93a10dd28186e5a6890d688906713f7f8
nikita <nikitacohen7@gmail.com>
2022-06-02 14:09:44 +0300

    mongoAdded

--- service/SearchService.js
+++ service/SearchService.js
@@ -1,5 +1,26 @@
 const {Client} = require('@elastic/elasticsearch');
 const client = new Client({node: 'http://localhost:9200'})
+const manualSchema = require("../module/ManualSchema");
+
+const addManual = (manual) => {
+    return new Promise((resolve, reject) => {
+        const newManual = new manualSchema({
+            "brand" : manual.brand,
+            "category" : manual.category,
+            "url" : manual.url,
+            "title" : manual.title,
+            "parsingDate" : new Date().toString()
+        })
+        newManual.save((err) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(newManual)
+            }
+        })
+    })
+}
+
 
 async function getManuals(word) {
     return new Promise(async (resolve, reject) => {
@@ -33,6 +54,7 @@ async function getAllManuals(word) {
 async function insertManual(manual) {
     return new Promise(async (resolve, reject) => {
         try {
+            await addManual(manual);
             const result = await client.create({
                 index: 'completeindexfour',
                 id : manual.title,
