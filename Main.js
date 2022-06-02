4c40e0a56be3c6f248b6fb55a697b815cb5a14fe
nikita <nikitacohen7@gmail.com>
2022-06-02 14:43:08 +0300

    mongoAdded

--- Main.js
+++ Main.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const searchRouter = require('./router/SearchRouter');
 
-require('./config/ConfigDB');
+require('./config/ConfigDB')
 
 app.use(express.json());
 app.use(cors());
