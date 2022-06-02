c67f2193448c1d676592a3298d0b39e7c48123a4
nikita <nikitacohen7@gmail.com>
2022-06-02 13:52:44 +0300

    change index

--- service/SearchService.js
+++ service/SearchService.js
@@ -1,5 +1,5 @@
 const {Client} = require('@elastic/elasticsearch');
-const client = new Client({node : 'http://localhost:9200' })
+const client = new Client({node: 'http://localhost:9200'})
 
 async function getManuals(word) {
     return new Promise(async (resolve, reject) => {
@@ -7,8 +7,8 @@ async function getManuals(word) {
             index: 'completeindexthree',
             from: 0,
             size: 30,
-            query :{
-                match : {title : word}
+            query: {
+                match: {title: word}
             }
         });
         resolve(result.hits.hits);
@@ -21,8 +21,8 @@ async function getAllManuals(word) {
             index: 'completeindexthree',
             from: 0,
             size: 500,
-            query :{
-                match : {title : word}
+            query: {
+                match: {title: word}
             }
         });
         resolve(result.hits.hits);
@@ -32,25 +32,22 @@ async function getAllManuals(word) {
 
 async function insertManual(manual) {
     return new Promise(async (resolve, reject) => {
-        const exists = await client.exists({
-            index: 'completeindexthree',
-            title : manual.title
-        });
-        if (!exists) {
-            const result = await client.index({
-                index : 'completeindexthree',
-                body : {
-                    brand : manual.brand,
-                    category : manual.category,
-                    url : manual.url,
-                    title : manual.title,
-                    parsingData : new Date().toString()
+        try {
+            const result = await client.create({
+                index: 'completeindexthree',
+                id : manual.title,
+                body: {
+                    brand: manual.brand,
+                    category: manual.category,
+                    url: manual.url,
+                    title: manual.title,
+                    parsingData: new Date().toString()
                 }
             })
             await client.indices.refresh({index: 'completeindexthree'})
             resolve(result);
-        } else {
-            reject("document already exists")
+        } catch (e) {
+            reject(e);
         }
     })
 }
