a5429e94189e53a14d0ccf5bc5b6bc85d9861f3a
nikita <nikitacohen7@gmail.com>
2022-06-15 16:52:52 +0300

    mongoAdded

--- service/SearchService.js
+++ service/SearchService.js
@@ -1,5 +1,5 @@
 const {Client} = require('@elastic/elasticsearch');
-const client = new Client({node: 'http://localhost:9200'})
+const client = new Client({node: 'http://localhost:9200', requestTimeout : Infinity})
 const manualSchema = require("../module/ManualSchema");
 
 const addManual = (manual) => {
